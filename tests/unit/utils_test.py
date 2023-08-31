6abffd4b6a28bd088dac24f37a0ff20f1a5f7de5
benf <12837887+007vasy@users.noreply.github.com>
2023-08-31 14:52:52 +1000

    parsing config into dataclasses

--- tests/unit/utils_test.py
+++ tests/unit/utils_test.py
@@ -1,4 +1,65 @@
-from utils import dummy
+from utils import (
+    parse_config,
+    Config,
+    File2NetworkMap,
+    Node2ColMap,
+    Edge2ColMap
+)
+import unittest
 
-def test_dummy():
-    assert dummy(1) == 2
+class TestConfig(unittest.TestCase):
+    def test_config_parse(self):
+        raw_config = {
+            'bq_exports': {
+                'output_*.json':{
+                    'nodes': {
+                        'id': 'id',
+                        'attributes': {
+                            'name': 'name',
+                            'type': 'type'
+                        }
+                    },
+                    'edges': {
+                        'source': 'source',
+                        'target': 'target',
+                        'attributes': {
+                            'type': 'type'
+                        }
+                    }
+                }
+            }
+        }
+
+        desired_config:Config = Config(
+            folder2networkmap={
+                'bq_exports': {
+                    'output_*.json':File2NetworkMap(
+                        nodes=Node2ColMap(
+                            id='id',
+                            attributes={
+                                'name': 'name',
+                                'type': 'type'
+                            }
+                        ),
+                        edges=Edge2ColMap(
+                            source='source',
+                            target='target',
+                            attributes={
+                                'type': 'type'
+                            }
+                        )
+                    )
+                }
+            }
+        )
+
+        actual_config = parse_config(raw_config)
+
+        self.assertEqual(actual_config, desired_config)
+
+class TestNodeFileGen(unittest.TestCase):
+    def test_singular_node_gen(self):
+        pass
+
+    def test_multiple_node_gen(self):
+        pass
--- toolbox/utils.py
+++ toolbox/utils.py
@@ -1,6 +1,10 @@
 import os
 import logging
 from pathlib import Path
+from dataclasses import dataclass
+import json
+
+from typing import Dict
 
 LOGLEVEL_KEY = 'LOGLEVEL'
 logging.basicConfig(
@@ -10,6 +14,71 @@ logging.basicConfig(
 )
 
 ROOT_DIR = Path(__file__).parent.parent
+
+ATTRIBUTES = Dict[str, str]
+
+
+@dataclass
+class Node2ColMap:
+    id: str
+    attributes: ATTRIBUTES
+
+
+@dataclass
+class Edge2ColMap:
+    source: str
+    target: str
+    attributes: ATTRIBUTES
+
+
+@dataclass
+class File2NetworkMap:
+    nodes: Node2ColMap
+    edges: Edge2ColMap
+
+
+FOLDER2NETWORKMAP = Dict[str, File2NetworkMap]
+
+
+@dataclass
+class Config:
+    folder2networkmap: FOLDER2NETWORKMAP
+
+
+def load_json(path: Path) -> Dict:
+    with open(path) as f:
+        return json.load(f)
+
+
+def parse_config(raw_config: Dict) -> Config:
+    folder2networkmap: Dict = {}
+    for folder, folder_networkmap in raw_config.items():
+        if folder not in folder2networkmap:
+            folder2networkmap[folder] = {}
+        for file, file_networkmap in folder_networkmap.items():
+            if file not in folder2networkmap[folder]:
+                folder2networkmap[folder][file] = {}
+            nodes = file_networkmap['nodes']
+            edges = file_networkmap['edges']
+            folder2networkmap[folder][file] = File2NetworkMap(
+                nodes=Node2ColMap(
+                    id=nodes['id'],
+                    attributes=nodes['attributes']
+                ),
+                edges=Edge2ColMap(
+                    source=edges['source'],
+                    target=edges['target'],
+                    attributes=edges['attributes']
+                )
+            )
+    return Config(
+        folder2networkmap=folder2networkmap
+    )
+
+
+def get_config(config_path: Path) -> Config:
+    raw_config = load_json(config_path)
+    return parse_config(raw_config)
 
 
 def dummy(a):
